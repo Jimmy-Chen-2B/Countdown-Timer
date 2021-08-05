b9d5adb9eda213a6bc530e831d0e27edc8d05222
Jimmy-Chen-2B <cyd08067@icloud.com>
2021-08-05 23:42:44 +0800

    finished

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   
-  let min = document.querySelector("#minute");
-  let sec = document.querySelector("#second");
-  let initSec = (+min.textContent * 60) + +sec.textContent;
+  const min = document.querySelector("#minute");
+  const sec = document.querySelector("#second");
+  const container = document.querySelector(".container")
+  const bell = document.querySelector("audio")
+  let initTime = (+min.textContent * 60) + +sec.textContent;
   let nInterval;
- 
+  let settingTime = "";
+
   function zeroPadding(num) {
     return num >= 10 ? num : "0" + num;
   }
@@ -14,51 +17,72 @@ document.addEventListener("DOMContentLoaded", () => {
     sec.textContent = zeroPadding(parseInt(time % 60));
   }  
 
+  function resetInterval() {
+    clearInterval(nInterval);
+    nInterval = 0;
+  }
+
   function startCountDown(duration) {
-      nInterval = setInterval(() => { 
+    nInterval = setInterval(() => { 
       duration--;
       sixtyRadix(duration);
-      if (duration === 0) {clearInterval(nInterval)};
+      if (duration === 0) {
+        clearInterval(nInterval)
+        bell.play()
+      };
+      container.style.cssText = `background: linear-gradient(90deg, pink ${100 - (duration / initTime * 100)}%, transparent 0%)`;
     }, 1000);
   };
 
   function pause() {
     let pauseTime = (+min.textContent * 60) + +sec.textContent;   
     if (nInterval) {
-      clearInterval(nInterval);
-      nInterval = 0;
+      resetInterval();
     } else { 
       startCountDown(pauseTime);
     }; 
   };
 
+  function adjustTime(event) {
+    sixtyRadix(event);  
+    resetInterval();
+    startCountDown(event);
+  }
+
   function rewindTime() {
     let rewind = (+min.textContent * 60) + +sec.textContent + 5;
-    if (rewind > initSec) {rewind = initSec};
-    clearInterval(nInterval);
-    nInterval = 0;
-    sixtyRadix(rewind);
-    rewind--;
-    sixtyRadix(rewind);
-    startCountDown(rewind);    
+    if (rewind > initTime) {rewind = initTime};
+    adjustTime(rewind);    
   }
 
   function fastForwardTime() {
     let fastForward = (+min.textContent * 60) + +sec.textContent - 5;
-    if (fastForward <= 0) {fastForward = 0};
-    clearInterval(nInterval);
-    nInterval = 0; 
-    sixtyRadix(fastForward);
-    fastForward--;
-    sixtyRadix(fastForward);
-    startCountDown(fastForward);     
+    if (fastForward < 0) {fastForward = 0};
+    adjustTime(fastForward);     
+  }
+
+  function defaultTime() {
+    adjustTime(initTime); 
+  }
+
+  function setTime(e) {
+    settingTime += e.key;
+    newTimer = Number(settingTime)
+    if (newTimer >= 60) {
+      alert("您輸入的數值超過上限(60)，請重新輸入");
+      settingTime = "";
+    } else if (newTimer) {
+      initTime = newTimer * 60;
+    } else {
+      alert("您輸入的數值有誤，請重新輸入");
+      settingTime = "";
+    }
   }
 
   window.addEventListener("keydown", (e) => {
-    // console.log(e.key);
 
     if (e.key === "Enter" && !nInterval) {
-      startCountDown(initSec);
+      startCountDown(initTime);
     };
 
     if (e.key === " ") {
@@ -66,14 +90,20 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     if (e.key === "ArrowLeft" && nInterval) {
-      rewindTime(e)
+      rewindTime();
     };
 
     if (e.key === "ArrowRight" && nInterval) {
-      fastForwardTime()
+      fastForwardTime();
     }
 
+    if (e.key === "ArrowUp" && nInterval) {
+      defaultTime();
+    }
 
+    if (+e.key || e.key === ".") {
+      setTime(e)
+    }
 
   });
 });
